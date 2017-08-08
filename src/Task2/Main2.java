84f94d4297e3ac2008688b03fab41f2164cbd96f
MaxPrimeAERY <30773045+MaxPrimeAERY@users.noreply.github.com>
2017-08-08 19:15:43 +0300

    Update for Task2

--- src/Task2/Main2.java
+++ src/Task2/Main2.java
@@ -5,12 +5,13 @@ package Task2;
  */
 public class Main2 {
     public static void main(String[] args) {
-        Triangle trioOne = new Triangle();
-        Triangle trioTwo = new Triangle();
-        double sqOne = trioOne.square(12, 16, 20);
+        Triangle trioOne = new Triangle(12,16,18);
+        Triangle trioTwo = new Triangle(2,4,20);
+        double sqOne = trioOne.square();
+
         System.out.println("S1 = " + sqOne + " cm^2");
         System.out.println();
-        double sqTwo = trioTwo.square(21.3, 27.44, 51.2);
+        double sqTwo = trioTwo.square();
         System.out.println("S2 = " + sqTwo + " cm^2");
     }
 }
--- src/Task2/Triangle.java
+++ src/Task2/Triangle.java
@@ -8,13 +8,43 @@ public class Triangle {
     private double b;
     private double c;
 
-
-    public double square(double a, double b, double c) {
+    public Triangle(double a, double b, double c) {
+        super();
         this.a = a;
         this.b = b;
         this.c = c;
+    }
 
-        double p = (a + b + c) / 2;
+    public Triangle() {
+        super();
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double square() {
+        double p = (this.a + this.b + this.c) / 2;
 
         if ((a + b) <= c || (a + c) <= b || (b + c) <= a) {
             System.out.println("This triangle doesn't exist");
